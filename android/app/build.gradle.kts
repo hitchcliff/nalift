5d63704becffe12a4cc25f9f28ab1f785cc16128
kevin700brands <kevin@700brands.com>
2025-05-29 03:30:09 +0800

    setup firestore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,29 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
+}
+
 android {
     namespace = "com.kevin.nalift"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,10 +36,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nalift"
+        applicationId = "com.kevin.nalift"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
+        minSdkVersion(23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- android/app/src/main/kotlin/com/example/nalift/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nalift/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nalift
+package com.kevin.nalift
 
 import io.flutter.embedding.android.FlutterActivity
 
