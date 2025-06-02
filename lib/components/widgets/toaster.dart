2e92821155e59e967b2adfe3b0316627ed404de8
hitchcliff <hitchcliff1@gmail.com>
2025-06-02 20:13:44 +0800

    toaster widget

---
+++ lib/components/widgets/toaster.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nalift/components/texts/body_text.dart';
+import 'package:nalift/constants/colors.dart';
+import 'package:nalift/services/navigation_service.dart';
+
+class Toaster {
+  // final _isDark = MyHelpers.isDarkMode(NavigationService().context!);
+
+  error(String msg) {
+    final snackBar = SnackBar(
+      content: BodyText(msg, color: MyColors.white),
+      backgroundColor: MyColors.googleColor,
+    );
+    ScaffoldMessenger.of(NavigationService().context!).showSnackBar(snackBar);
+  }
+
+  success(String msg) {
+    final snackBar = SnackBar(
+      content: BodyText(msg, color: MyColors.white),
+      backgroundColor: MyColors.verified,
+    );
+    ScaffoldMessenger.of(NavigationService().context!).showSnackBar(snackBar);
+  }
+}
