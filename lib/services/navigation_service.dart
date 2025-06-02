36e58211dfc7677c2e049927786aad8c884e578a
hitchcliff <hitchcliff1@gmail.com>
2025-06-02 20:17:11 +0800

    make pop method future

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,5 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class NavigationService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  BuildContext? get context => NavigationService.navigatorKey.currentContext;
+
+  Future<bool> pop(int wait) async {
+    await Future.delayed(
+      Duration(seconds: wait),
+      () => Navigator.pop(context!),
+    );
+    return true;
+  }
 }
